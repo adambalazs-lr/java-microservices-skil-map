f209b49741a13aa087c82a835b4cf8fe08079ab8
Adam Balazs <adam.balazs@liferay.com>
2020-09-07 11:38:00 +0200

    2A - rename application.yml to bootstrap.yml to let spring use cloud config's application.yml

--- rest-service/src/main/java/com/example/restservice/CloudConfigPropertiesConfigurationProperties.java
+++ rest-service/src/main/java/com/example/restservice/CloudConfigPropertiesConfigurationProperties.java
@@ -21,12 +21,6 @@ public class CloudConfigPropertiesConfigurationProperties {
         this.hello = hello;
     }
 
-    //TODO: ask why am I cannot to retrive rest-service-prod.yaml, why this
-    // **** falling back to rest-service.yaml. This looking suspicious:
-    // 2020-09-05 14:24:52.576  INFO 24660 --- [           main] c.c.c.ConfigServicePropertySourceLocator : Fetching config from server at : http://localhost:8888
-    // 2020-09-05 14:24:53.610  INFO 24660 --- [           main] c.c.c.ConfigServicePropertySourceLocator : Located environment: name=rest-service, profiles=[default], label=null, version=24fb26d7a95ea38f25840344229f83a4859b0a83, state=null
-    // 2020-09-05 14:24:53.610  INFO 24660 --- [           main] b.c.PropertySourceBootstrapConfiguration : Located property source: [BootstrapPropertySource {name='bootstrapProperties-configClient'}, BootstrapPropertySource {name='bootstrapProperties-https://github.com/adambalazs-lr/spring-boot-config-demo.git/application.properties'}]
-    // I tried many solutions with no avail yet.
     private String foo;
 
     private String hello;
